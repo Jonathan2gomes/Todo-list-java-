7c11cc334ea87a0ad7b1b63cf2e2b624ea76fc28
jonathan2gomes <jonathan2gomes.jg@gmail.com>
2020-02-14 19:49:32 -0300

    uploading to right project

---
+++ src/main/java/com/eventoapp/EventoappApplication.java
@@ -0,0 +1,26 @@
+package com.eventoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+import static org.springframework.boot.SpringApplication.*;
+
+
+@SpringBootApplication
+public class EventoappApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		run(EventoappApplication.class, args);
+		}
+
+}
